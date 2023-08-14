cf64155524c844fb6231312c6b4ce4164c7cc48c
Joshuakme <joshuakoh23@gmail.com>
2023-08-14 14:46:08 +0800

    Update navigation bar from Gym.Master

--- GymWeb/Account.aspx
+++ GymWeb/Account.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Title="Account" Language="C#" MasterPageFile="~/Gym.Master" AutoEventWireup="true" CodeBehind="Account.aspx.cs" Inherits="GymWeb.Account" %>
+﻿<%@ Page Title="Account | FTF Gym Web" Language="C#" MasterPageFile="~/Gym.Master" AutoEventWireup="true" CodeBehind="Account.aspx.cs" Inherits="GymWeb.Account" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
--- GymWeb/Gym.Master.designer.cs
+++ GymWeb/Gym.Master.designer.cs
@@ -33,13 +33,13 @@ namespace GymWeb
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// LogoImg control.
+        /// logoImg control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Image LogoImg;
+        protected global::System.Web.UI.WebControls.Image logoImg;
 
         /// <summary>
         /// SiteMapPath1 control.
@@ -49,15 +49,6 @@ namespace GymWeb
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.SiteMapPath SiteMapPath1;
-
-        /// <summary>
-        /// Menu1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Menu Menu1;
 
         /// <summary>
         /// SiteMapDataSource control.
