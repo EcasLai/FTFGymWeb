1182ec2eb39ed188f1d1ca5fa1950dc41736b507
Joshuakme <joshuakoh23@gmail.com>
2023-08-14 21:14:37 +0800

    Update Account page - left sidebar

--- GymWeb/Account.aspx.designer.cs
+++ GymWeb/Account.aspx.designer.cs
@@ -13,5 +13,41 @@ namespace GymWeb
 
     public partial class Account
     {
+
+        /// <summary>
+        /// AccountIcon control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image AccountIcon;
+
+        /// <summary>
+        /// OrderIcon control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image OrderIcon;
+
+        /// <summary>
+        /// Repeater1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater Repeater1;
+
+        /// <summary>
+        /// SqlDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
     }
 }
