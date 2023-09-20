02a9a29bf5d2e7f290795b8c60898f59bd6ee479
Joshua Koh <joshuakoh23@gmail.com>
2023-09-21 01:19:16 +0800

    Update Login & SignUp Pages

--- GymWeb/Login.aspx.cs
+++ GymWeb/Login.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,6 +14,28 @@ namespace GymWeb
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnLogIn_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString))
+            {
+                SqlCommand cmd = new SqlCommand("SELECT count(1) FROM Customer WHERE email=@email AND password=@password", con);
+
+                cmd.Parameters.AddWithValue("@email", txtEmail.Text);
+                cmd.Parameters.AddWithValue("@password", txtPassword.Text);
+
+                int count = Convert.ToInt32(cmd.ExecuteScalar());
+                if(count == 1)
+                {
+                    Session["email"] = txtEmail.Text.Trim();
+                    Response.Redirect("Homepage.aspx");
+                }
+                else
+                {
+                    ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Incorrect User Credentials')", true);
+                }
+            }
         }
     }
 }
